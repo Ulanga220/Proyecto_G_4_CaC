a12cd0a72591f5811a8190044b0f257a7d2f828d
Ulanga220 <ulises.ferreyra@hotmail.com>
2022-05-21 18:52:40 -0300

    Cambios para entrega

--- js/login.js
+++ js/login.js
@@ -1,24 +1,36 @@
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = [...document.getElementsByClassName('needs-validation')];
+    const email = document.getElementById('emailUsuario');
+    const password = document.getElementById('contraseñaUsuario');
+    const rememberMe = document.getElementById('remindEmail');
+    // Loop over them and prevent submission
 
-(function () {
-    'use strict'
-  
+    email.value = this.localStorage.getItem('email')
 
-    var forms = document.querySelectorAll('.needs-validation')
-  
- 
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
+    forms.forEach(form => {
+      form.addEventListener('submit', function(event) {
+        console.log(email.value);
+        console.log(password.value);
+        console.log(rememberMe.value);
+        if (email.value && password.value) {        
+          form.classList.add('was-validated');
+
+          if(rememberMe){
+            localStorage.setItem('email', email.value);
           }
-  
-          form.classList.add('was-validated')
-          
-        }, false)
-      })
-  })()
 
+          if(email.value == 'pepe@gmail.com' && password.value == 'pepe123'){
+            localStorage.setItem('usuario', email.value);
+            window.location = '/login.html';
+          }
+        }
 
-  const productos = localStorage.getItem("productos");
+        event.preventDefault();
+        event.stopPropagation();
+      }, false);
+    });
+  }, false);
+})();
